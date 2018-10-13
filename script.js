c02dc1add78f2ad0db92a6450e22e5360264932e
Syedkhizer <38943055+syedkhizerazmi@users.noreply.github.com>
2018-10-13 17:33:14 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+let price=5.99;
+let count=0;
+let cost= 5.99
+var num = 5.99;
+var n = num.toFixed(2);
+function max(){
+  
+    document.querySelector(".num").value=count++;
+  
+    document.querySelector(".prize").innerHTML=count*(num).toFixed(2);
+}
+function min(){
+
+    let prize=0;
+    let numb=0;
+    document.querySelector(".num").value=count--;
+    
+    document.querySelector(".prize").innerHTML=count*-cost;
+      if(price=numb){
+        // document.querySelector(".prize").innerHTML.("");  
+    }
+  
+      
+}
